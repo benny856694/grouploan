5521cf885f4d1a8d2a14161e2f522bf2dad87ee3
benny <shenbinsc@qq.com>
2021-12-22 15:55:09 +0800

    'update'

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,9 +19,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "0e7c10d71f127534fa7c29bbbab94a47",
-"/": "0e7c10d71f127534fa7c29bbbab94a47",
-"main.dart.js": "d6a8a9c36244f4534502c59e08b71d99",
+"index.html": "043260bc133d929a20b3c93c946b229d",
+"/": "043260bc133d929a20b3c93c946b229d",
+"main.dart.js": "35740576959ab621398a3e44e0fe3842",
 "manifest.json": "7f4699cfdf4061183cac09fe02faaa01",
 "version.json": "80e7fe95099fda638cab1dfbc4496d70"
 };
