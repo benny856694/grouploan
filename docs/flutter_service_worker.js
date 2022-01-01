ffd3cffc80a256f5ff3d7d60adbba066b1ba1ce6
benny856694 <shenbinsc@qq.com>
2022-01-02 01:22:49 +0800

    publish

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -5,9 +5,9 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "80e7fe95099fda638cab1dfbc4496d70",
 "splash/style.css": "64227ec06c71fef909f75868ada72c30",
-"index.html": "d0fc5761234675bfab12cf57d9c127d4",
-"/": "d0fc5761234675bfab12cf57d9c127d4",
-"main.dart.js": "1bae009e35b8ecc851b48186ddbb2d0e",
+"index.html": "113ccaaeb89605bf5dc7e4afc1b74093",
+"/": "113ccaaeb89605bf5dc7e4afc1b74093",
+"main.dart.js": "002a2f20b04920726fe89ff19f07aa7f",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
