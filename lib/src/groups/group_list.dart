4c2e47a4778d447613786956944296982844ae77
benny856694 <shenbinsc@qq.com>
2022-01-01 23:57:31 +0800

    cosmetic

--- lib/src/groups/group_list.dart
+++ lib/src/groups/group_list.dart
@@ -69,140 +69,146 @@ class _GroupListState extends State<GroupList> {
             Text(
               isEdit ? 'Edit Group' : labelAddGroup,
             ),
-            const SizedBox(
-              width: 5,
-            ),
-            OnReactive(() {
-              if (appState.groups.isWaiting) {
-                return const SizedBox.square(
-                  dimension: 18,
-                  child: CircularProgressIndicator(
-                    strokeWidth: 2,
-                  ),
-                );
-              }
-              return const SizedBox.shrink();
-            })
           ],
         ),
-        content: SizedBox(
-          width: 400,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              TextField(
-                focusNode: nameFocusNode,
-                autofocus: true,
-                controller: nameEditController,
-                decoration: const InputDecoration(
-                  labelText: 'Name',
-                ),
-              ),
-              TextField(
-                controller: accountNumberEditController,
-                decoration: const InputDecoration(
-                  labelText: 'Account No.',
-                ),
-              ),
-              TextField(
-                controller: leaderNameEditController,
-                decoration: const InputDecoration(
-                  labelText: 'Leader Name',
-                ),
-              ),
-              TextField(
-                controller: phoneNumberEditController,
-                decoration: const InputDecoration(
-                  labelText: labelTel,
-                ),
-              ),
-              DateTimePicker(
-                type: DateTimePickerType.date,
-                initialValue: registrationDate.toString(),
-                firstDate: DateTime(2000),
-                lastDate: DateTime(2100),
-                dateLabelText: 'Registration Date',
-                timeLabelText: "Hour",
-                onChanged: (value) {
-                  registrationDate = DateTime.parse(value);
-                },
-                onSaved: (val) {
-                  registrationDate = DateTime.parse(val!);
-                },
-              ),
-              Row(
-                children: [
-                  //latitude text field
-                  Expanded(
-                    child: TextField(
-                      controller: latitudeEditController,
-                      decoration: const InputDecoration(
-                        labelText: 'Latitude',
-                      ),
+        content: Stack(
+          children: [
+            SizedBox(
+              width: 400,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  TextField(
+                    focusNode: nameFocusNode,
+                    autofocus: true,
+                    controller: nameEditController,
+                    decoration: const InputDecoration(
+                      labelText: 'Name',
                     ),
                   ),
-                  const SizedBox(width: 8),
-                  //longitude text field
-                  Expanded(
-                    child: TextField(
-                      controller: longitudeEditController,
-                      decoration: const InputDecoration(
-                        labelText: 'Longitude',
-                      ),
+                  TextField(
+                    controller: accountNumberEditController,
+                    decoration: const InputDecoration(
+                      labelText: 'Account No.',
                     ),
                   ),
-                  //icon button to get location
-                  if (!_isGettingLocation)
-                    IconButton(
-                      icon: const Icon(Icons.my_location),
-                      onPressed: () async {
-                        setState(() {
-                          _isGettingLocation = true;
-                        });
-                        try {
-                          var location = await appState.getLocation();
-                          setState(() {
-                            _isGettingLocation = false;
-                            if (location?.longitude != null) {
-                              longitudeEditController.text =
-                                  //4 digit precision
-                                  location!.longitude!.toStringAsFixed(4);
-                              latitudeEditController.text =
-                                  location.latitude!.toStringAsFixed(4);
+                  TextField(
+                    controller: leaderNameEditController,
+                    decoration: const InputDecoration(
+                      labelText: 'Leader Name',
+                    ),
+                  ),
+                  TextField(
+                    controller: phoneNumberEditController,
+                    decoration: const InputDecoration(
+                      labelText: labelTel,
+                    ),
+                  ),
+                  DateTimePicker(
+                    type: DateTimePickerType.date,
+                    initialValue: registrationDate.toString(),
+                    firstDate: DateTime(2000),
+                    lastDate: DateTime(2100),
+                    dateLabelText: 'Registration Date',
+                    timeLabelText: "Hour",
+                    onChanged: (value) {
+                      registrationDate = DateTime.parse(value);
+                    },
+                    onSaved: (val) {
+                      registrationDate = DateTime.parse(val!);
+                    },
+                  ),
+                  Row(
+                    children: [
+                      //latitude text field
+                      Expanded(
+                        child: TextField(
+                          controller: latitudeEditController,
+                          decoration: const InputDecoration(
+                            labelText: 'Latitude',
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      //longitude text field
+                      Expanded(
+                        child: TextField(
+                          controller: longitudeEditController,
+                          decoration: const InputDecoration(
+                            labelText: 'Longitude',
+                          ),
+                        ),
+                      ),
+                      //icon button to get location
+                      if (!_isGettingLocation)
+                        IconButton(
+                          icon: const Icon(Icons.my_location),
+                          onPressed: () async {
+                            setState(() {
+                              _isGettingLocation = true;
+                            });
+                            try {
+                              var location = await appState.getLocation();
+                              setState(() {
+                                _isGettingLocation = false;
+                                if (location?.longitude != null) {
+                                  longitudeEditController.text =
+                                      //4 digit precision
+                                      location!.longitude!.toStringAsFixed(4);
+                                  latitudeEditController.text =
+                                      location.latitude!.toStringAsFixed(4);
+                                }
+                              });
+                            } catch (e) {
+                              RM.scaffold.showSnackBar(SnackBar(
+                                content: Text(e.toString()),
+                              ));
+                            } finally {
+                              setState(() {
+                                _isGettingLocation = false;
+                              });
                             }
-                          });
-                        } catch (e) {
-                          RM.scaffold.showSnackBar(SnackBar(
-                            content: Text(e.toString()),
-                          ));
-                        } finally {
-                          setState(() {
-                            _isGettingLocation = false;
-                          });
-                        }
-                      },
-                    )
-                  else
-                    const SizedBox.square(
-                      dimension: 18,
-                      child: CircularProgressIndicator(
-                        strokeWidth: 2,
-                      ),
-                    ),
-                  //icon button to show in map
-                  if (longitudeEditController.text.isNotEmpty)
-                    IconButton(
-                      icon: const Icon(Icons.map),
-                      onPressed: () async {
-                        var url =
-                            'https://www.google.com/maps/search/?api=1&query=${latitudeEditController.text},${longitudeEditController.text}';
-                        await launch(url);
-                      },
-                    ),
+                          },
+                        )
+                      else
+                        const SizedBox.square(
+                          dimension: 18,
+                          child: CircularProgressIndicator(
+                            strokeWidth: 2,
+                          ),
+                        ),
+                      //icon button to show in map
+                      if (longitudeEditController.text.isNotEmpty)
+                        IconButton(
+                          icon: const Icon(Icons.map),
+                          onPressed: () async {
+                            var url =
+                                'https://www.google.com/maps/search/?api=1&query=${latitudeEditController.text},${longitudeEditController.text}';
+                            await launch(url);
+                          },
+                        ),
+                    ],
+                  )
                 ],
-              )
-            ],
-          ),
+              ),
+            ),
+            OnReactive(
+              () {
+                if (appState.groups.isWaiting) {
+                  return Positioned.fill(
+                    child: Container(
+                      color: Theme.of(context)
+                          .dialogBackgroundColor
+                          .withOpacity(0.8),
+                      alignment: Alignment.center,
+                      child: const CircularProgressIndicator(),
+                    ),
+                  );
+                }
+                return const SizedBox.shrink();
+              },
+            ),
+          ],
         ),
         actions: <Widget>[
           if (!isEdit)
