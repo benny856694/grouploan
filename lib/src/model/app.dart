819a9d46db4bcbf6557e32c1fe290ca7c2748e31
benny856694 <shenbinsc@qq.com>
2022-01-18 00:50:19 +0800

    use CRUD

--- lib/src/model/app.dart
+++ lib/src/model/app.dart
@@ -1,12 +1,14 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:group_loan/src/model/group.dart';
+import 'package:group_loan/src/model/group_repository.dart';
 import 'package:location/location.dart';
 import 'package:states_rebuilder/states_rebuilder.dart';
 
 class AppState {
-  final groups = RM.inject<List<Group>>(
-    () => [],
+  final groups = RM.injectCRUD<Group, GroupParam>(
+    () => GroupRepository(),
+    param: () => GroupParam(countToRead: 20),
+    readOnInitialization: true,
     sideEffects: SideEffects.onAll(
       onData: null,
       onWaiting: null,
@@ -23,56 +25,7 @@ class AppState {
   );
 
   final _locator = Location();
-  var _initialized = false;
 
-  late final CollectionReference<Map<String, dynamic>> _groupsRef;
-
-  AppState() : _groupsRef = FirebaseFirestore.instance.collection('groups');
-
-  Future<List<Group>> addGroup(Group group) {
-    return groups.setState(
-      (s) async {
-        var doc = await _groupsRef.add(group.toMap());
-        group.id = doc.id;
-        return [...s, group];
-      },
-    );
-  }
-
-  void removeGroup(Group group) {
-    groups.setState((s) async {
-      await _groupsRef.doc(group.id).delete();
-      return s.where((g) => g.id != group.id).toList();
-    });
-  }
-
-  void updateGroup(Group group) {
-    groups.setState((s) async {
-      await _groupsRef.doc(group.id).update(group.toMap());
-      return s.map((g) => g.id == group.id ? group : g).toList();
-    });
-  }
-
-  Future removeGroups(List<Group> groups) async {
-    this.groups.setState((s) async {
-      await Future.wait(groups.map((g) => _groupsRef.doc(g.id).delete()));
-      return s.where((g) => !groups.contains(g)).toList();
-    });
-  }
-
-  void loadGroups() {
-    if (groups.state.isEmpty && !_initialized) {
-      _initialized = true;
-      groups.setState((s) async {
-        var snapshot = await _groupsRef.limit(20).get();
-        return snapshot.docs.map((doc) {
-          var g = Group.fromMap(doc.data());
-          g.id = doc.id;
-          return g;
-        }).toList();
-      });
-    }
-  }
 
   Future<LocationData?> getLocation() async {
     bool serviceEnabled;
