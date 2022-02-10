21a46af5de00bf12f8c7f0dd14e2d09ffa6846c4
benny <shenbinsc@qq.com>
2022-02-10 11:42:45 +0800

    refactor

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -5,6 +5,7 @@ import 'package:flutterfire_ui/auth.dart';
 import 'package:group_loan/src/auth/authgate.dart';
 import 'package:group_loan/src/staffs/staffs.dart';
 
+import 'auth/signin.dart';
 import 'groups/group_list.dart';
 import 'sample_feature/sample_item_details_view.dart';
 import 'sample_feature/sample_item_list_view.dart';
@@ -69,8 +70,6 @@ class MyApp extends StatelessWidget {
           // Define a function to handle named routes in order to support
           // Flutter web url navigation and deep linking.
           onGenerateRoute: (RouteSettings routeSettings) {
-            const providers = [EmailProviderConfiguration()];
-
             return MaterialPageRoute<void>(
               settings: routeSettings,
               builder: (BuildContext context) {
@@ -87,57 +86,7 @@ class MyApp extends StatelessWidget {
                     return const Groups();
                   default:
                     return FirebaseAuth.instance.currentUser == null
-                        ? SignInScreen(
-                            providerConfigs: providers,
-                            showAuthActionSwitch: false,
-                            headerBuilder: (context, constraints, _) {
-                              return Padding(
-                                padding: const EdgeInsets.all(20),
-                                child: AspectRatio(
-                                  aspectRatio: 1,
-                                  child: Image.asset(
-                                    'assets/images/logo.jpeg',
-                                  ),
-                                ),
-                              );
-                            },
-                            sideBuilder: (context, constraints) {
-                              return Padding(
-                                padding: const EdgeInsets.all(20),
-                                child: AspectRatio(
-                                  aspectRatio: 1,
-                                  child: Image.asset(
-                                    'assets/images/logo.jpeg',
-                                  ),
-                                ),
-                              );
-                            },
-                            subtitleBuilder: (context, action) {
-                              return const Padding(
-                                padding: EdgeInsets.only(bottom: 8),
-                                child: Text(
-                                    'Welcome to Group Loan! Please sign in to continue.'),
-                              );
-                            },
-                            footerBuilder: (context, action) {
-                              return const Padding(
-                                padding: EdgeInsets.only(top: 16),
-                                child: Text(
-                                  'By signing in, you agree to our Terms of Service and Privacy Policy.',
-                                  style: TextStyle(color: Colors.grey),
-                                ),
-                              );
-                            },
-                            actions: [
-                              AuthStateChangeAction<SignedIn>(
-                                (context, _) {
-                                  Navigator.of(context).pushReplacementNamed(
-                                    Groups.routeName,
-                                  );
-                                },
-                              ),
-                            ],
-                          )
+                        ? const SignIn()
                         : const Groups();
                 }
               },
