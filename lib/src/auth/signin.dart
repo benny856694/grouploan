ab36974ad73580b85756cda18f191054f948aefc
benny <shenbinsc@qq.com>
2022-02-18 12:34:30 +0800

    cosmetic: signin screen

--- lib/src/auth/signin.dart
+++ lib/src/auth/signin.dart
@@ -13,55 +13,57 @@ class SignIn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SignInScreen(
-      providerConfigs: const [
-        EmailProviderConfiguration(),
-      ],
-      showAuthActionSwitch: false,
-      headerBuilder: (context, constraints, _) {
-        return Padding(
-          padding: const EdgeInsets.all(20),
-          child: AspectRatio(
-            aspectRatio: 1,
-            child: Image.asset(
-              'assets/images/logo.jpeg',
+    return Scaffold(
+      body: SignInScreen(
+        providerConfigs: const [
+          EmailProviderConfiguration(),
+        ],
+        showAuthActionSwitch: false,
+        headerBuilder: (context, constraints, _) {
+          return Padding(
+            padding: const EdgeInsets.all(20),
+            child: AspectRatio(
+              aspectRatio: 1,
+              child: Image.asset(
+                'assets/images/logo.jpeg',
+              ),
             ),
-          ),
-        );
-      },
-      sideBuilder: (context, constraints) {
-        return Padding(
-          padding: const EdgeInsets.all(20),
-          child: AspectRatio(
-            aspectRatio: 1,
-            child: Image.asset(
-              'assets/images/logo.jpeg',
+          );
+        },
+        sideBuilder: (context, constraints) {
+          return Padding(
+            padding: const EdgeInsets.all(20),
+            child: AspectRatio(
+              aspectRatio: 1,
+              child: Image.asset(
+                'assets/images/logo.jpeg',
+              ),
             ),
+          );
+        },
+        subtitleBuilder: (context, action) {
+          return const Padding(
+            padding: EdgeInsets.only(bottom: 8),
+            child: Text('Welcome to Group Loan! Please sign in to continue.'),
+          );
+        },
+        footerBuilder: (context, action) {
+          return const Padding(
+            padding: EdgeInsets.only(top: 16),
+            child: Text(
+              'By signing in, you agree to our Terms of Service and Privacy Policy.',
+              style: TextStyle(color: Colors.grey),
+            ),
+          );
+        },
+        actions: [
+          AuthStateChangeAction<SignedIn>(
+            (context, _) {
+              myNavigator.toReplacement(Groups.routeName);
+            },
           ),
-        );
-      },
-      subtitleBuilder: (context, action) {
-        return const Padding(
-          padding: EdgeInsets.only(bottom: 8),
-          child: Text('Welcome to Group Loan! Please sign in to continue.'),
-        );
-      },
-      footerBuilder: (context, action) {
-        return const Padding(
-          padding: EdgeInsets.only(top: 16),
-          child: Text(
-            'By signing in, you agree to our Terms of Service and Privacy Policy.',
-            style: TextStyle(color: Colors.grey),
-          ),
-        );
-      },
-      actions: [
-        AuthStateChangeAction<SignedIn>(
-          (context, _) {
-            myNavigator.toReplacement(Groups.routeName);
-          },
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
